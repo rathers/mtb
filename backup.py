df70b215d29fae412b919492bc024e278ededbb5
Dan Rathbone <dan@rathers.co.uk>
2012-02-26 21:55:48 +0000

    Source directory structure now preserved
    takes source path and trims off the bottom level directory
    This is used to start a new directory tree in the destination
    Source directory then recursed and files copied into their corresponding
    directories in the dest
    
    Also added some commenting

--- backup.py
+++ backup.py
@@ -13,6 +13,7 @@ import glob, threading, Queue, sys, shutil, time, os.path
 
 
 class Worker(threading.Thread):
+    dest = "/temp/backup_test"
     def __init__(self, q):
         threading.Thread.__init__(self)
         self.daemon=True
@@ -22,25 +23,40 @@ class Worker(threading.Thread):
         while True:
             # get something off the queue i
             # (will block forever if neccessary)
-            source = self._q.get()
-            if not os.path.isfile(source):
+            (srcFile, relDir) = self._q.get()
+            if not os.path.isfile(srcFile):
                 self._q.task_done()
                 continue
-            print '{}: {}'.format(self.name, source)
+            destDir  = self.dest + os.sep + relDir
+            (srcRoot, srcFilename) = os.path.split(srcFile)
+            destFile = destDir + os.sep + srcFilename
+            if not os.path.exists(destDir):
+                try:
+                    # makedirs not thread safe be prepared for exception
+                    # if another thread has already created this dir
+                    os.makedirs(destDir, 0755) 
+                except OSError, err: 
+                    if err.errno==17: #file exists 
+                        # someone else has created the directory in the 
+                        # meantime. That's fine with me! 
+                        pass 
+                    else: 
+                        raise
+            print '{}: Copying from {} to {}'.format(self.name, srcFile, destFile)
             try:
-                #shutil.copy2(source,'/temp/backup_test/')
+                shutil.copy2(srcFile, destFile)
                 a=1
             except IOError as e:
                 print '   ... Skipping: {}'.format(e)
             self._q.task_done()
 
 class Producer(object):
+    noThreads = 5
     def __init__(self, q):
         self._q = q
     def start(self):
-        noThreads=5
         # create N worker threads to consume the queue
-        self.startWorkers(noThreads, self._q)
+        self.startWorkers(self.noThreads, self._q)
         self.readFilesFile('files.txt', self._q)
         self.waitForEnd(self._q)
     def waitForEnd(self, q):
@@ -61,23 +77,33 @@ class Producer(object):
         # resolves wildchars in the path
         resolvedPaths = glob.glob(line)
         for path in resolvedPaths:
+            # it's a directory
             if os.path.isdir(path):
+                # walk the direcotry passing the original path 
+                # and the queue to the walker function as a tuple
                 os.path.walk(path, self.processDir, (q, path))
+            # it's just a file
             else:
                 self.putWithRetry(path, q)
     def processDir(self, (q,origPath), dirname, names):
+        # queue and origPath unpacked frm tuple
+        # split FQP on original filename that was found in the files file
         (root,basename) = os.path.split(origPath)
+        # now find relative path. 
+        # This will ultimatey be used as the dir path in s3
         relDir = os.path.relpath(dirname, root)
         for f in names:
             fqPath = os.path.join(dirname, f)
-            print 'Got {}, {}'.format(relDir, fqPath)
+            #print 'Got {}, {}'.format(relDir, fqPath)
+            # not interested in blank directories
             if os.path.isdir(fqPath):
                 continue
-            self.putWithRetry(fqPath, q)
-    def putWithRetry(self, path, q):
+            # shove on queue
+            self.putWithRetry(fqPath, relDir, q)
+    def putWithRetry(self, fqPath, relDir, q):
         while True:
             try:
-                q.put(path, True, 1)
+                q.put((fqPath, relDir), True, 1)
                 break
             except Queue.Full:
                 a=1
